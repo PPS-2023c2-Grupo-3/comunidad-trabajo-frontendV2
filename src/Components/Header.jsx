537fdc18fb2677e6d5e89d6d968258d98bba03e8
Gonzalo09 <gonza.iglesias00@gmail.com>
2023-09-12 23:47:27 -0300

    Idea de nuevo header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,4 +1,3 @@
-import * as React from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
@@ -39,3 +38,143 @@ const Header = () => {
 };
 
 export default Header;
+
+{
+  /*
+  import {
+  AppBar,
+  Box,
+  Button,
+  Divider,
+  Grid,
+  IconButton,
+  InputBase,
+  Link,
+  Paper,
+  Toolbar,
+} from "@mui/material";
+import { useState } from "react";
+import SearchIcon from "@mui/icons-material/Search";
+
+const Header = () => {
+  const Buscador = () => {
+    const [buscador, setBuscador] = useState("");
+
+    const handleChange = (e) => {
+      setBuscador(e.target.value);
+    };
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      console.log(buscador);
+    };
+
+    return (
+      <Paper
+        component="form"
+        sx={{
+          p: "2px 4px",
+          display: "flex",
+          alignItems: "center",
+          width: "100%",
+          height: "50px",
+          borderRadius: "5px",
+        }}
+      >
+        <InputBase
+          sx={{
+            ml: 2,
+            flex: 1,
+            fontSize: "1rem",
+          }}
+          placeholder="Buscar empleo..."
+          value={buscador}
+          onChange={handleChange}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              handleSubmit(e);
+            }
+          }}
+        />
+        <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+        <IconButton
+          type="button"
+          sx={{ p: "10px", color: "#009688" }}
+          aria-label="search"
+          onClick={handleSubmit}
+        >
+          <SearchIcon />
+        </IconButton>
+      </Paper>
+    );
+  };
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar
+            sx={{
+              padding: "10px",
+            }}
+          >
+            <Grid
+              container
+              item
+              xs={3}
+              justifyContent="flex-start"
+              alignItems="center"
+              alignContent="center"
+            >
+              <Link to="/">
+                <img
+                  src="https://cdn.discordapp.com/attachments/956988369693454466/989600731369709669/Logoblanco.png"
+                  alt="logo"
+                  style={{
+                    height: "4rem",
+                    width: "auto",
+                    padding: "5px",
+                    marginTop: "5px",
+                    alignContent: "center",
+                  }}
+                />
+              </Link>
+            </Grid>
+            <Grid
+              container
+              item
+              xs={6}
+              justifyContent="center"
+              alignItems="center"
+            >
+              <Buscador />
+            </Grid>
+            <Grid
+              container
+              item
+              xs={3}
+              justifyContent="flex-end"
+              alignItems="center"
+            >
+              <Button
+                variant="outlined"
+                color="inherit"
+                href="/"
+                size="small"
+                sx={{
+                  borderRadius: "5px",
+                  padding: "8px",
+                }}
+              >
+                Iniciar sesión
+              </Button>
+            </Grid>
+          </Toolbar>
+        </AppBar>
+      </Box>
+  );
+};
+
+export default Header;
+
+  */
+}
